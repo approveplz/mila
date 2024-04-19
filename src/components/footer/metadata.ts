cd473f3295d39a48acee72dba6fa34f402d68aa7
Ahmed Ejaz <Ahmed.Ejaz@aegis-ps.com>
2024-04-19 22:21:25 +0500

    Implement responsive footer component with navigation links and copyright information

---
+++ src/components/footer/metadata.ts
@@ -0,0 +1,9 @@
+export const navigation = [
+    { name: 'How it works', href: '/' },
+    { name: 'FAQ', href: '/' },
+    { name: 'About us', href: '/' }
+]
+
+export const data = {
+    copyRights:'&copy; Mila Collective 2024'
+}
