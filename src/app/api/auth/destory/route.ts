bc12088440b9c0f1e66fe9ff02d623b41013794e
juttameerhamza <juttameerhamza@gmail.com>
2024-05-25 13:14:20 +0500

    route handler remove

--- src/app/api/auth/destory/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { auth, signOut } from "@/auth"
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    const session = await auth(req, res)
-}
