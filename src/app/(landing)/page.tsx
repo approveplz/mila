aa8aa0d2c8976dcb8ef281b935d3e190214c7fe2
Muhammad Hanif Sarwari <hanifsarwari.nuces@gmail.com>
2024-06-09 21:53:30 +0500

    benefits section removed

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
     <>
       <Header />
       <Brands />
-      <Benefits />
+      {/* <Benefits /> */}
       <HowItWorks />
       <GiveAway />
       <Pricing session={session} products={products} />
