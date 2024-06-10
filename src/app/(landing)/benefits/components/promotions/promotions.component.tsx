79e8a0fb06464c5d497cd50b1124897e48a1ed67
Ahmad Aijaz <ahmadaijaz1030@gmail.com>
2024-06-10 17:06:33 +0500

    Completed image integration

--- src/app/(landing)/benefits/components/promotions/promotions.component.tsx
+++ src/app/(landing)/benefits/components/promotions/promotions.component.tsx
@@ -33,9 +33,7 @@ export function Promotions() {
             <Image
               src={width > 640 ? "/images/BannerAmaraSacs(desktop).jpg" : '/images/BannerAmaraSacs(mobile).jpg'}
               alt="promotions"
-              layout="cover"
-              width={319}
-              height={267}
+              layout="fill"
               className={`w-full object-cover`}
             />
           </SwiperSlide>
@@ -44,31 +42,10 @@ export function Promotions() {
             <Image
               src={width > 640 ? "/images/BannerRevival(Desktop).jpg" : '/images/BannerRevival(mobile).jpg'}
               alt="promotions"
-              layout="cover"
-              width={319}
-              height={267}
+              layout="fill"
               className={`w-full object-cover`}
             />
           </SwiperSlide>
-          {/* {[1, 2, 3, 4].map(item => (
-            <SwiperSlide key={item} className='relative'>
-
-              <div className='sm:absolute flex sm:flex-col flex-row justify-center items-center gap-2 w-full sm:w-[174px] sm:h-[223px] bg-[#171614] py-[22px] px-6 sm:py-6 sm:px-8 sm:rounded-[24px] sm:top-6 sm:right-28'>
-                <div className='font-tt-ramillas text-2xl text-[#FFFFFF]'>ITALIC</div>
-                <div className='font-normal leading-[26px] text-[19px] sm:leading-8 sm:text-2xl text-[#FFFFFF]'>UPTO</div>
-                <div className='font-black text-[38.79px] leading-[38.79px] sm:text-5xl sm:leading-[48px] text-[#FFFFFF]'>50% OFF</div>
-              </div>
-
-              <Image
-                src="/images/promotions.png"
-                alt="promotions"
-                // layout="cover"
-                width={319}
-                height={267}
-                className={`w-full`}
-              />
-            </SwiperSlide>
-          ))} */}
         </Swiper>
 
         <button className="hidden sm:flex arrow-left absolute top-[124px] z-20 left-6 p-2 bg-[#FFFFFF] shadow-lg rounded-full"><HiArrowSmallLeft size={24} /></button>
